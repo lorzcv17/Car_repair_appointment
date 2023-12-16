aa2b50f10c011431686fbeb127b728a096f4545c
Lorelie Villarba <147228330+lorzcv17@users.noreply.github.com>
2023-12-17 01:12:32 +0800

    Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.admin')
+@section('content')
+<div class="content">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 color="black" class='Lucida Console' style='font-size:24px'>  HOME </h1>
+        <img src="{{ URL ('images/car.jpg')}}" alt="car repair" width="800" height="800" class="center" >
+        </div>
+    </div>
+</div>
+@endsection
+@section('scripts')
+@parent
+
+@endsection
